dd42f8d215ee59e70a34e2ed7f35f959abaa96a9
Claus Kühnel <info@ckuehnel.ch>
2022-06-22 06:33:53 +0200

    Update arduino_secrets.h

--- ESP32/ESP32_ePulse/arduino_secrets.h
+++ ESP32/ESP32_ePulse/arduino_secrets.h
@@ -5,5 +5,5 @@ const char* ssid = "Sunrise_2.4GHz_8AC2A0";
 const char* pw   = "u2xxxxxxxxx1Ds";
 
 // Initialize Telegram BOT
-#define BOTtoken "1481682315:AAEqbGjYlKqLTnA-ZkM5qk-u0eJenDvtyQY"  // your Bot Token (Get from Botfather)
+#define BOTtoken "<Your Token>"  // your Bot Token (Get from Botfather)
 #define CHAT_ID "929477751"
